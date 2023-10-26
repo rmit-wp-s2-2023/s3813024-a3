c093818f7f8e23582d38d542ae8775ceaaa1d18f
Abdulrahman Alanazi <s3813024@student.rmit.edu.au>
2023-10-26 17:13:38 +1100

    fixing issues

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    
+    <link rel="stylesheet" href="a3.css">
+    
+</head>
+<body>
+    <h1>Orders</h1>
+    <?php require_once("parts/functions.php");?>
+   
+
+    <hr>
+    <?php require_once("parts/sitemap.php") ?>
+  </body>
+</html>
